fdda8e383039fa70c7f5d11a48312006ef6ad2c7
oceanlvr <657531018@qq.com>
2024-07-08 21:08:55 +0800

    com

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import HomePage from './HomePage';
 import VideoPage from './VideoPage';
+import Video1 from './assets/1.mp4';
+import Video2 from './assets/2.mp4';
+import Video3 from './assets/3.mp4';
+import Video4 from './assets/4.mp4';
+
 import './App.css';
 
 const App = () => {
   const navigate = useNavigate();
-
+  const videos = [Video1, Video2, Video3, Video4];
+  const paths = [ '/video1', '/video2', '/video3', '/video4'];
+  const config = videos.map((video, index) => ({ video, path: paths[index] }));
   useEffect(() => {
     const handleKeyDown = (event) => {
       switch (event.key) {
@@ -37,10 +44,11 @@ const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
-      <Route path="/video1" element={<VideoPage videoSrc="1.mp4" />} />
-      <Route path="/video2" element={<VideoPage videoSrc="2.mp4" />} />
-      <Route path="/video3" element={<VideoPage videoSrc="3.mp4" />} />
-      <Route path="/video4" element={<VideoPage videoSrc="4.mp4" />} />
+      {
+        config.map(({ video, path }) => (
+          <Route key={path} path={path} element={<VideoPage videoSrc={video} />} />
+        ))
+      }
     </Routes>
   );
 };
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-// import HomeImg from '../home.png'
 import './home.less'
 
 const HomePage = () => {
   return (
-    <div className='home-img' ></div>
+    <div className='home-img'></div>
   );
 };
 
